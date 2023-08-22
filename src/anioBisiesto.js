f8f14bf0c23efe84156b2fc15c7242f0fdb63dc7
“Cristian <cristiancanapi730@gmail.com>
2023-08-22 02:19:14 -0400

    Anios divisibles entre 400 son bisiestos

--- src/anioBisiesto.js
+++ src/anioBisiesto.js
@@ -1,5 +1,7 @@
 function esBisiesto(año) {
-    
+    if (año % 400 === 0) {
+        return true;
+    }
 }
 
 export default esBisiesto;
--- src/anioBisiesto.test.js
+++ src/anioBisiesto.test.js
@@ -1,5 +1,7 @@
 import esBisiesto from "./anioBisiesto";
 
 describe("esBisiesto", () => {
-  
+    it("debería devolver true para años divisibles por 400", () => {
+        expect(esBisiesto(2000)).toEqual(true);
+      });
 });
