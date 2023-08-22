30bd83a345d4ab95852eff6aa016cca6734db18d
“Cristian <cristiancanapi730@gmail.com>
2023-08-22 02:30:29 -0400

    Tercer punto del año bisiesto implementado exitosamente

--- src/anioBisiesto.js
+++ src/anioBisiesto.js
@@ -3,6 +3,8 @@ function esBisiesto(año) {
         return true;
     } else if (año % 100 === 0 && año % 400 != 0) {
         return false;
+    } else if (año % 4 === 0 && año % 100 != 0) {
+        return true;
     }
 }
 
--- src/anioBisiesto.test.js
+++ src/anioBisiesto.test.js
@@ -7,4 +7,7 @@ describe("esBisiesto", () => {
     it("debería devolver false para años divisibles por 100 pero no por 400", () => {
         expect(esBisiesto(1900)).toEqual(false);
       });
+    it("debería devolver true para años divisibles por 4 pero no por 100", () => {
+        expect(esBisiesto(2012)).toEqual(true);
+      });
 });
